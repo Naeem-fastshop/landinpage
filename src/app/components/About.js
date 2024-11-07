e2397d625e7c872ad60c8b40da893d398f475885
Fast Shop <naeem@fast-shop.co.uk>
2024-11-07 18:24:49 +0500

    change name to bilal

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -43,7 +43,7 @@ const About = ({ courses }) => {
             <div key={course.id} className="p-6 bg-white rounded-lg shadow-lg">
               <h4 className="text-xl font-semibold mb-2 text-black">{course.title}</h4>
               <p className="text-gray-600">{course.body}</p>
-              <p className="text-gray-500 mt-2">Instructor: khan</p>
+              <p className="text-gray-500 mt-2">Instructor: Bilal</p>
               <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
                 Enroll Now
               </button>
